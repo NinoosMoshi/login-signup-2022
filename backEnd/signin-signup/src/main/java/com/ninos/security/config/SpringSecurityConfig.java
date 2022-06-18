e83b6c434050efb09d92078ade7b5cffac2f4f0f
NinoosMoshi <nalkaton@gmail.com>
2022-06-18 08:46:10 -0400

    social with spring Closes #25

--- backEnd/signin-signup/src/main/java/com/ninos/security/config/SpringSecurityConfig.java
+++ backEnd/signin-signup/src/main/java/com/ninos/security/config/SpringSecurityConfig.java
@@ -43,6 +43,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/activated").permitAll()
                 .antMatchers("/check-email").permitAll()
                 .antMatchers("/resetPassword").permitAll()
+                .antMatchers("/social/**").permitAll()
                 .anyRequest().authenticated();
 
     }
